58b107a9ced9cec44070297e4bff4a549c5c0f25
MouadBNL <mouadbnl.2k01@gmail.com>
2025-10-11 14:20:11 +0100

    feat: testing the export

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import { UnoCard } from "./components/uno-card/uno-card"
 import { UnoSet } from "./constants/uno-set"
+import * as htmlToImage from 'html-to-image'
 function App() {
+
+  const downloadCard = async (name: string) => {
+    const card = document.getElementById(name);
+    if (!card) return;
+    const dataUrl = await htmlToImage.toPng(card, {
+      backgroundColor: 'transparent',
+    });
+    console.log(dataUrl);
+    const link = document.createElement('a');
+    link.href = dataUrl;
+    link.target = '_blank';
+    link.click();
+  }
   return (
     <>
       <div>
         <h1>Hello World</h1>
         {UnoSet.map((card) => (
-          <div style={{ width: "10%" }}>
+          <div style={{ width: "10%" }} key={card.name}>
+            <div>
+              <button onClick={() => downloadCard(card.name)}>Download</button>
+            </div>
             <UnoCard key={card.name} config={card} />
           </div>
         ))}
--- src/components/uno-card/uno-card.tsx
+++ src/components/uno-card/uno-card.tsx
@@ -5,7 +5,7 @@ import "./uno-card.css";
 export function UnoCard({ config }: { config: UnoCardConfig }) {
   const image = "https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=";
   return (
-    <div className="uno-card-container" >
+    <div className="uno-card-container" id={config.name} >
       <UnoCardIcon svg={config.icon()} position="top-left" />
       <UnoCardIcon svg={config.icon()} position="bottom-right" />
       <div className="uno-card-inner" style={{ backgroundColor: config.backgroundColor }}>
@@ -15,6 +15,13 @@ export function UnoCard({ config }: { config: UnoCardConfig }) {
           <path d="M75.8894 2.59058C90.54 11.201 87.561 43.458 69.2357 74.6386C50.9103 105.819 24.1779 124.116 9.52731 115.506C-5.12332 106.895 -2.14435 74.6382 16.181 43.4575C34.5064 12.2769 61.2388 -6.01984 75.8894 2.59058Z" fill="#EAEAEC" />
         </svg>
       </div>
+
+      {/* This needs to be present in every component for the export to work with the clip path*/}
+      <svg width="0" height="0" style={{ position: "absolute" }}>
+        <defs>
+          <clipPath id="ellipsis-path" clipPathUnits="objectBoundingBox"><path d="M 0.893 0.022 C 1.043 0.087 1.049 0.367 0.815 0.637 C 0.599 0.903 0.292 1.041 0.112 0.986 C -0.06 0.912 -0.025 0.637 0.19 0.371 C 0.406 0.105 0.721 -0.051 0.893 0.022"></path></clipPath>
+        </defs>
+      </svg>
 
       <div className="uno-card-content" style={{ backgroundImage: `url(${image})` }}>
       </div>
