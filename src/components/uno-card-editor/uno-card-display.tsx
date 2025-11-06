b0aad770cb3e1e9255bbd14d9bbcb4ba17b09638
MouadBNL <mouadbnl.2k01@gmail.com>
2025-11-06 15:49:29 +0100

    fix: ts errors

--- src/components/uno-card-editor/uno-card-display.tsx
+++ src/components/uno-card-editor/uno-card-display.tsx
@@ -4,12 +4,12 @@ import { useUnoCardEditorContext } from "./uno-card-context";
 
 
 export function UnoCardDisplay() {
-  const { cards, setSelectedCard, selectedCard, size } = useUnoCardEditorContext();
+  const { cards, setSelectedCard, selectedCard, card: cardConfig } = useUnoCardEditorContext();
   return (
     <div className="flex flex-wrap gap-8 justify-between p-8">
       {cards.map((card) => (
         <div key={card.name} className={cn("p-1 cursor-pointer border-2 rounded-md border-gray-200", selectedCard?.name === card.name && "border-primary")} onClick={() => setSelectedCard(card)}>
-          <UnoCard config={card} size={size} />
+          <UnoCard config={card} width={`${cardConfig.width}cm`} height={`${cardConfig.height}cm`} />
         </div>
       ))}
     </div>
